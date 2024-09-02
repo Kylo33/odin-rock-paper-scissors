e5f1b15c3ae410ff2790815dbd17e8354b612bdb
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2024-09-02 10:23:38 -0600

    feat: add function to play rock paper scissors round

--- script.js
+++ script.js
@@ -30,3 +30,41 @@ function getHumanChoice() {
 
     return input;
 }
+
+// This function capitalizes the first letter of a string
+// It assumes that the string is at least one character long.
+function capitalizeFirstLetter(string) {
+    return string[0].toUpperCase() + string.substring(1);
+}
+
+function playRound(humanChoice, computerChoice) {
+    // If humanChoice is equal to computer choice, print a tie message and return
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+        return;
+    }
+    // Declare a humanWon variable
+    // If humanChoice is rock and computerChoice is scissors
+        // or if humanChoice is scissors and computerChoice is paper
+        // or if humanChoice is paper and computerChoice is rock
+            // Assign True to humanWon
+    let humanWon = (humanChoice === "rock" && computerChoice === "scissors") ||
+                    (humanChoice === "scissors" && computerChoice === "paper") ||
+                    (humanChoice === "paper" && computerChoice === "rock");
+
+
+    // If humanWon
+        // print a victory message
+        // increment human score by 1
+    // Else
+        // print a loss message
+        // increment computer score by 1
+
+    if (humanWon) {
+        console.log(`Victory! ${capitalizeFirstLetter(humanChoice)} beats ${capitalizeFirstLetter(computerChoice)}.`)
+        humanScore++;
+    } else {
+        console.log(`You lose! ${capitalizeFirstLetter(computerChoice)} beats ${capitalizeFirstLetter(humanChoice)}.`)
+        computerScore++;
+    }
+}
