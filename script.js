5d3609c42c0cf3a6fefa143c28965b684f451a8c
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2024-09-02 10:13:27 -0600

    feat: add score variables

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice() {
     // Generate a random number between 0 and 2 (inclusive)
     let randomNumber = Math.floor(Math.random() * 3);
