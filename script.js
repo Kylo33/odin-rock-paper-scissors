c19350771e855ff7ba5d8cc77f0402c86b99d4d2
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2024-09-02 10:07:55 -0600

    feat: add random computer choice function

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+    // Generate a random number between 0 and 2 (inclusive)
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    // If the number is 0, return "rock"
+    // Else if the number is 1, return "paper"
+    // Else return "scissors"
+
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
